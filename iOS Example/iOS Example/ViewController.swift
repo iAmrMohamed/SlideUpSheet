d900e58bf373d3f4a646d7aa42f757e44912ff16
Amr Mohamed <iAmrMohamed@gmail.com>
2023-02-11 14:46:57 +0200

    setup initial iOS Example project

---
+++ iOS Example/iOS Example/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  iOS Example
+//
+//  Created by Amr Mohamed on 10/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
